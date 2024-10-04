5d1d293d2fd4b5564622b8275cb3c724b5f2760f
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-05 02:57:25 +0500

    parameters for the print in python

--- Basic/Print_Input.py
+++ Basic/Print_Input.py
@@ -1,5 +1,11 @@
 print('HELLO WORLD')
 # Input A Name In Python
 name=input('ENTER A NAME ')
+
 # Showcasing the value of the variable in the print function
 print(f"The name is {name}")
+
+#parameters in print
+'''
+print('whatever you want', sep ="we can enter anything as a seperator, end=we can enter how we end the print )
+'''
