483a32f74e48a75ce29f368c94cc92fa4f66e7d7
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-05 03:01:43 +0500

    basic string methods are tested and told

---
+++ Basic/Basic_String_Methods.py
@@ -0,0 +1,18 @@
+# Input A Name In Python
+name=input('ENTER A NAME ')
+
+#strip is a string method that removes all white spaces
+name=name.strip()
+print(f"The name is {name}")
+
+
+#capitalize just makes the first letter of the first word capital
+name=name.capitalize()
+print(f"The name is {name}")
+
+#title is another string method that makes the first letter of each word capital
+name=name.title()
+
+# Showcasing the value of the variable in the print function
+print(f"The name is {name}")
+
