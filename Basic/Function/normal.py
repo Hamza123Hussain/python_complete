f0068e0d8f635150529c5c5e1b7f76897f4e281d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-06 19:01:44 +0500

    This is the code, in which are calling other functions but main function is not there.

---
+++ Basic/Function/normal.py
@@ -0,0 +1,8 @@
+def Hello(Name="hamza"): #How Normal Function is Defined In Python
+    print(F"THE NAME OF THE USER {Name}") # Body Of The Function
+    
+    
+Hello('lujjain') #Calling the Function    
+
+'''IF we want another function then we have to define it above the calling code everytime.
+ If the code is longer then this could be a problem.'''
