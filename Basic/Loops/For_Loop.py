4dbe3a78bb6f4b7059d0eee0571405c817808e84
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-07 01:55:23 +0500

    Basic For Loop in python

---
+++ Basic/Loops/For_Loop.py
@@ -0,0 +1,12 @@
+Number=int(input('Enter A Number : '))
+
+for i in [0,1,2]: # simple way of using a for loop.
+ # give starting value, step and ending value   
+    print(f"Number Here : {i}")
+    
+    
+ #Using For Loop Using Range we can give a number via input or even hard code the range
+for _ in range(3):
+     print(f"Number Here : {i}")
+     
+     
