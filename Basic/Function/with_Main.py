e66a784732f903f9cb92cfe5eb996602593b0524
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-06 19:05:03 +0500

    Using Return In A Function

--- Basic/Function/with_Main.py
+++ Basic/Function/with_Main.py
@@ -3,6 +3,8 @@ def Main():
     Add All The Body Of The Code Within it.'''
     Name=input('Enter A Name')
     Hello(Name)
+    Number=int(input('Enter A Number'))
+    print (f"The Number Has Been Squared : {SQAURE(Number)}")
     
     
 '''We Just Have Make The Main Function At The Top
@@ -10,6 +12,10 @@ And Then We Can Make The Other Functions AnyWhere We Want To
 And Can Acess Those Functions In the Main Function Body.'''    
 def Hello(Name="hamza"): #How Normal Function is Defined In Python
     print(F"THE Name Of The User -> {Name}") # Body Of The Function
+    
+    
+def SQAURE(Number): #How Normal Function is Defined In Python
+    return Number * Number
         
         
 Main()        
