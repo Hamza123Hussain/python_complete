21fe0e59c00f62b3894a13b27448ac922feaf05b
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-06 18:24:10 +0500

    basic int and float functions completed

---
+++ Basic/Int_Float.py
@@ -0,0 +1,12 @@
+# CONVERTING STRING TO A NUMBER AND THEN ADDING THEM UP
+Number=int(input('Enter Number 1  : '))
+Number2=int(input('Enter Number 2 : '))
+print(f"THE SUM OF BOTH : {Number+Number2}")
+
+#converting num 1 to float. the sum would come in float format as it has higher precedence
+Number=float(input('Enter Number 1  : '))
+Number2=int(input('Enter Number 2 : '))
+print(f"THE SUM OF BOTH : {Number+Number2}")
+
+#Rounding off a number
+print(f"ROUND OFF NUMBER : {round((Number))}")
