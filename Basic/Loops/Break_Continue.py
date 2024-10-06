c2f30001981a0a2750779918dcbc70266ca60c8d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-07 01:55:26 +0500

    Create Break_Continue.py

---
+++ Basic/Loops/Break_Continue.py
@@ -0,0 +1,7 @@
+while True:
+    Number=int(input('Enter A Number : '))
+    if(Number<0):
+        continue #tHE inPUT WOULD CONTIUNE TILL CONDITION IS BROKEN
+    else:
+        break # LOOP STOPS AS CONDTION IS BROKEN
+print(F"NUMBER IS HERE {Number}")    
