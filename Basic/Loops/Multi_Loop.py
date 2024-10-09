bce909de7f8f7408d73ee0279962266a76b79e8d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-09 18:03:09 +0500

    Basic Multi Loop in Python

---
+++ Basic/Loops/Multi_Loop.py
@@ -0,0 +1,8 @@
+Amount=int(input('Enter How Many Sqaures To Be Printed : '))
+
+# Simple Functioning Of A Multi Loop
+for i in range(Amount):
+    for j in range(Amount):
+        print('#'*j,end='')
+    print()    
+        
