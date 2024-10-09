ddf2707a73861b6f7b2a34fd83edc4ada2562f26
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-09 17:47:25 +0500

    A very basic impletion of a dictionary in python

---
+++ Basic/Dic_List/Dictionary.py
@@ -0,0 +1,6 @@
+#A simple dictionary made in python
+simple_dictionary={'Name':'HAMZA','Full_Name':"Hamza Hussain",'Age':24,'Gender':'Male'}
+
+# Iterating through a dictionary
+for elemenet in simple_dictionary:
+    print('ELEMENT OF DIC',elemenet)
