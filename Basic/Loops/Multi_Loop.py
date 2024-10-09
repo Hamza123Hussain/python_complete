8e8902107258aa59410883c839fa5e847af159e5
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-09 18:07:29 +0500

    simplified multiple loop code

--- Basic/Loops/Multi_Loop.py
+++ Basic/Loops/Multi_Loop.py
@@ -3,6 +3,10 @@ Amount=int(input('Enter How Many Sqaures To Be Printed : '))
 # Simple Functioning Of A Multi Loop
 for i in range(Amount):
     for j in range(Amount):
-        print('#'*j,end='')
-    print()    
+        print('#',end='')
+    print()
+    
+# A Cleaner Version Of The Multi Loop
+for i in range(Amount):
+    print('#'*Amount)        
         
