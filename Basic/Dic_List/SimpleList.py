3d17aec3d61a0c036cf69bf0e248aa26567daf54
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-10-11 21:33:29 +0500

    PLACED IN CORRECT FILE

--- Basic/Dic_List/SimpleList.py
+++ Basic/Dic_List/SimpleList.py
@@ -3,3 +3,10 @@ for Number in FirstList: # simple for loop working over an array
     print('NUMBER ',Number)
     
 print('Length Of An Array',len(FirstList)) # This is the length of an array
+names=[]
+for _ in range(3):
+    names.append(input('Enter Name : '))
+    
+names=sorted(names)
+
+print('Sorted Names : ',names)    
